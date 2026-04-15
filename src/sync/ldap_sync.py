b406b247d8c03e46ec33d536119972646dfa5575
NathanEmanuel <nathan.emanuel.d@proton.me>
2026-04-15 12:54:21 +0200

    Implement unoptimized full sync

--- src/sync/ldap_sync.py
+++ src/sync/ldap_sync.py
@@ -1,5 +1,8 @@
+import logging
+
 from congressus import CongressusClient
 from congressus.models import Group as CongressusGroup
+from congressus.models import GroupMembership as CongressusGroupMembership
 from congressus.models import Member as CongressusMember
 from ldap import LdapClient
 from ldap.models import Group as LdapGroup
@@ -20,13 +23,39 @@ class LdapSync:
         self._ldap = ldap_client
         self._ldap_model_factory = ldap_model_factory
 
-    async def sync_groups(self, group_ids: list[int]) -> None:
+    async def sync_all(self, dry_run: bool = False) -> None:
+        active_committees = await self._congressus.list_active_committees()
+        committee_ids = [int(committee.get_id()) for committee in active_committees]
+        await self.sync_groups(committee_ids, dry_run=dry_run)
 
-        congressus_groups = await self._congressus.retrieve_groups(group_ids)
-        for congressus_group in congressus_groups:
+    async def sync_groups(self, group_ids: list[int], dry_run: bool = False) -> None:
+        async for congressus_group in self._congressus.retrieve_groups(group_ids):
 
-            ldap_group = self.create_group(congressus_group, ignore_existing=True)
-            await self.sync_group_memberships(ldap_group)
+            if not dry_run:
+                ldap_group = self.create_group(congressus_group, ignore_existing=True)
+            else:
+                ldap_group = self._ldap_model_factory.create_group_from_congressus_group(congressus_group)
+
+            await self._sync_group_memberships(ldap_group, congressus_group.memberships, dry_run=dry_run)
+            logging.info(f"Synced group: {congressus_group.name} (ID: {congressus_group.id})")
+
+    async def _sync_group_memberships(
+        self, ldap_group: LdapGroup, congressus_group_memberships: list[CongressusGroupMembership], dry_run: bool = False
+    ) -> None:
+        for membership in congressus_group_memberships:
+
+            if not membership.is_active():
+                continue
+
+            member = await self._congressus.retrieve_member(membership.member_id)
+            if not member.is_active():
+                continue
+
+            if not dry_run:
+                ldap_user = self.create_account(member)
+                self._ldap.add_to_group(ldap_user, ldap_group)
+            else:
+                logging.info(f"Would add member {member.first_name} {member.last_name} to group {ldap_group.get_name()}")
 
     def create_group(self, congressus_group: CongressusGroup, ignore_existing: bool = False) -> LdapGroup:
         ldap_group = self._ldap_model_factory.create_group_from_congressus_group(congressus_group)
@@ -38,13 +67,6 @@ class LdapSync:
         user.set_random_password_if_unset()
         self._ldap.create(user)
         return user
-
-    async def sync_group_memberships(self, ldap_group: LdapGroup) -> None:
-        congressus_members = await self._congressus.list_groups_active_members(int(ldap_group.get_id()))
-        for congressus_member in congressus_members:
-
-            ldap_user = self.create_account(congressus_member)
-            self._ldap.add_to_group(ldap_user, ldap_group)
 
     # region Context Manager
 
